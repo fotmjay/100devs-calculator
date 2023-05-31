56e963033f375f521e66a7e99bfebcb5ca6ecb39
fotmjay <95885719+fotmjay@users.noreply.github.com>
2023-05-31 11:59:57 -0400

    updated code to set calculator as a class, kept original code commented. also set up prettier.rc

--- js/main.js
+++ js/main.js
@@ -2,57 +2,163 @@ const calculatorBody = document.querySelector("section");
 const resultPanel = document.querySelector("#result");
 let numberStored = 0;
 let operation = "";
-
 resultPanel.value = "";
 
-calculatorBody.addEventListener("mouseup", clickedCalc);
+//  MODIFICATIONS TO MAKE IT OOP FROM HERE
+// function CreateCalculator() {
 
-function clickedCalc(click) {
-  if (isClickValid(click)) {
-    const target = click.target.innerText;
-    if (isOperation(target)) {
-      operatorUsed(target);
-    } else if (target === OPERATIONS.CLEAR) {
+//     this.clickedCalc = function(click) {
+//       if (isClickValid(click)) {
+//         const target = click.target.innerText;
+//         if (isOperation(target)) {
+//           operatorUsed(target);
+//         } else if (target === OPERATIONS.CLEAR) {
+//           resultPanel.value = "";
+//           numberStored = 0;
+//         } else {
+//           resultPanel.value += target;
+//         }
+//       }
+//     }
+
+//   function isOperation(target) {
+//     return (
+//       target === OPERATIONS.MULTIPLY ||
+//       target === OPERATIONS.DIVIDE ||
+//       target === OPERATIONS.ADD ||
+//       target === OPERATIONS.SUBSTRACT ||
+//       target === OPERATIONS.EQUALS
+//     );
+//   }
+
+//   function isClickValid(click){
+//     return (
+//       click.target.tagName === ACTIONS.OPERATIONS ||
+//       click.target.tagName === ACTIONS.CLEAR
+//     );
+//   }
+
+//   function operatorUsed(ope) {
+//     let currentValue = resultPanel.value;
+//     if (ope !== OPERATIONS.EQUALS) {
+//       operation = ope;
+//       numberStored = currentValue;
+//       resultPanel.value = "";
+//     } else {
+//       if (operation === OPERATIONS.MULTIPLY) {
+//         resultPanel.value = +numberStored * +currentValue;
+//       } else if (operation === OPERATIONS.DIVIDE) {
+//         resultPanel.value = +numberStored / +currentValue;
+//       } else if (operation === OPERATIONS.SUBSTRACT) {
+//         resultPanel.value = +numberStored - +currentValue;
+//       } else {
+//         resultPanel.value = +numberStored + +currentValue;
+//       }
+//     }
+//   }
+// }
+
+// function clickedCalc(click) {
+//   if (isClickValid(click)) {
+//     const target = click.target.innerText;
+//     if (isOperation(target)) {
+//       operatorUsed(target);
+//     } else if (target === OPERATIONS.CLEAR) {
+//       resultPanel.value = "";
+//       numberStored = 0;
+//     } else {
+//       resultPanel.value += target;
+//     }
+//   }
+// }
+
+// function operatorUsed(ope) {
+//   let currentValue = resultPanel.value;
+//   if (ope !== OPERATIONS.EQUALS) {
+//     operation = ope;
+//     numberStored = currentValue;
+//     resultPanel.value = "";
+//   } else {
+//     if (operation === OPERATIONS.MULTIPLY) {
+//       resultPanel.value = +numberStored * +currentValue;
+//     } else if (operation === OPERATIONS.DIVIDE) {
+//       resultPanel.value = +numberStored / +currentValue;
+//     } else if (operation === OPERATIONS.SUBSTRACT) {
+//       resultPanel.value = +numberStored - +currentValue;
+//     } else {
+//       resultPanel.value = +numberStored + +currentValue;
+//     }
+//   }
+// }
+
+// function isOperation(target) {
+//   return (
+//     target === OPERATIONS.MULTIPLY ||
+//     target === OPERATIONS.DIVIDE ||
+//     target === OPERATIONS.ADD ||
+//     target === OPERATIONS.SUBSTRACT ||
+//     target === OPERATIONS.EQUALS
+//   );
+// }
+
+// function isClickValid(click) {
+//   return (
+//     click.target.tagName === ACTIONS.OPERATIONS ||
+//     click.target.tagName === ACTIONS.CLEAR
+//   );
+// }
+
+class Calculator {
+  clickedCalc = (click) => {
+    if (this.isClickValid(click)) {
+      const target = click.target.innerText;
+      if (this.isOperation(target)) {
+        this.operatorUsed(target);
+      } else if (target === OPERATIONS.CLEAR) {
+        resultPanel.value = "";
+        numberStored = 0;
+      } else {
+        resultPanel.value += target;
+      }
+    }
+  };
+
+  isOperation = (target) => {
+    return (
+      target === OPERATIONS.MULTIPLY ||
+      target === OPERATIONS.DIVIDE ||
+      target === OPERATIONS.ADD ||
+      target === OPERATIONS.SUBSTRACT ||
+      target === OPERATIONS.EQUALS
+    );
+  };
+
+  isClickValid = (click) => {
+    return (
+      click.target.tagName === ACTIONS.OPERATIONS ||
+      click.target.tagName === ACTIONS.CLEAR
+    );
+  };
+
+  operatorUsed = (ope) => {
+    let currentValue = resultPanel.value;
+    if (ope !== OPERATIONS.EQUALS) {
+      operation = ope;
+      numberStored = currentValue;
       resultPanel.value = "";
-      numberStored = 0;
     } else {
-      resultPanel.value += target;
+      if (operation === OPERATIONS.MULTIPLY) {
+        resultPanel.value = +numberStored * +currentValue;
+      } else if (operation === OPERATIONS.DIVIDE) {
+        resultPanel.value = +numberStored / +currentValue;
+      } else if (operation === OPERATIONS.SUBSTRACT) {
+        resultPanel.value = +numberStored - +currentValue;
+      } else {
+        resultPanel.value = +numberStored + +currentValue;
+      }
     }
-  }
+  };
 }
 
-function operatorUsed(ope) {
-  let currentValue = resultPanel.value;
-  if (ope !== OPERATIONS.EQUALS) {
-    operation = ope;
-    numberStored = currentValue;
-    resultPanel.value = "";
-  } else {
-    if (operation === OPERATIONS.MULTIPLY) {
-      resultPanel.value = +numberStored * +currentValue;
-    } else if (operation === OPERATIONS.DIVIDE) {
-      resultPanel.value = +numberStored / +currentValue;
-    } else if (operation === OPERATIONS.SUBSTRACT) {
-      resultPanel.value = +numberStored - +currentValue;
-    } else {
-      resultPanel.value = +numberStored + +currentValue;
-    }
-  }
-}
-
-function isOperation(target) {
-  return (
-    target === OPERATIONS.MULTIPLY ||
-    target === OPERATIONS.DIVIDE ||
-    target === OPERATIONS.ADD ||
-    target === OPERATIONS.SUBSTRACT ||
-    target === OPERATIONS.EQUALS
-  );
-}
-
-function isClickValid(click) {
-  return (
-    click.target.tagName === ACTIONS.OPERATIONS ||
-    click.target.tagName === ACTIONS.CLEAR
-  );
-}
+const calculator = new Calculator();
+calculatorBody.addEventListener("mouseup", calculator.clickedCalc);
